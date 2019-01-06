201811ee7c2784374d320fea72dd234edac41b2e
oxygengamestudio <lucasdamour@icloud.com>
2019-01-06 21:40:51 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,29 +14,77 @@ const options = {
     channels: ["#cooxybot"]
 };
 
+
+// Create a client with our options
 const client = new tmi.client(options);
 
-// Connect the client to the server..
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
 client.connect();
 
-client.on("connected", function (address, port){
-    console.log("Adress: " + address + " Port: " + port);
-});
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
 
-client.on("chat",  (channel, user, message, self) => {
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
 
-    if (self) return;
-    let sender = user['display-name']
+  // If the command is known, let's execute it
+  if (commandName === '!liens') {
+    client.say(target, `Facebook: https://www.facebook.com/MasterSnakou | Twitter: https://twitter.com/MasterSnakou | Youtube: https://www.youtube.com/user/MasterSnakou | Youtube Replay: https://www.youtube.com/user/SnakouReplay | Instagram: https://www.instagram.com/mastersnakou | Snapchat: https://www.snapchat.com/add/mastersnakou`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
 
-    if(user['mod'] === false){
-        if(message.includes("www.") || message.includes(".com") || message.includes(".fr")){
-            client.timeout(channel, sender, 30, "Pas de liens :) !")
-        }
-    }
+  if (commandName === '!discord') {
+    client.say(target, `Rejoins le Discord ici: https://discord.gg/snakou (⚠️ Pense à lire le salon #accueil pour accepter les règles et avoir accès à tous les salons !)`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
 
-    if(message === "!hi"){
-        client.action("cooxybot","Coucou")
-    }
+  if (commandName === '!prime') {
+    client.say(target, `imGlitch Tu veux t'abonner tous les mois sans pour autant débourser 5€ par mois? La solution : TWITCH PRIME : http://wzb.lv/AmazonPrime C'est un abonnement (GRATUIT LE PREMIER MOIS) que tu payes 25€ par an (pour les moins de 25 ans) et qui te permet de t'abonner tous les mois à 1 chaîne Twitch de ton choix. Tu as aussi des jeux et plein de bonus gratuits tous les mois! :) DO IT! \o/`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
 
+  if (commandName === '!wallpaper') {
+    client.say(target, ` utilise Wallpaper Engine disponible sur STEAM: http://wzb.lv/5c14434c0`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
 
-});
+  if (commandName === '!miel') {
+    client.say(target, `Achète du Miel Goadien ici: http://www.unesaisonauxabeilles.com`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+
+  if (commandName === '!uptime') {
+    client.say(target, `Attend`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+
+  if (commandName === '!sub') {
+    client.say(target, ` Pour Sub et rejoindre les rangs: https://www.twitch.tv/subs/mastersnakou`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
