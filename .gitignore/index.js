3aa01828db73c9514f91282f9296c7baf59e0ad5
oxygengamestudio <lucasdamour@icloud.com>
2019-01-15 09:21:03 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -25,25 +25,21 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
-client.on("chat", (channel, user, message, self) => {
-
-  if (self) return;
-  let sender = user['display-name'];
-
-  if(message.includes("www.") || message.includes(".com")){
-    client.timeout(channel, sender, 60, "Lien Détecté");
-    client.action(channel, "Pas de lien");
-  }
-
-}
-
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
+    
+    if (self) return;
+  let sender = user['display-name'];
 
+  if(message.includes("www.") || message.includes(".com")){
+    client.timeout(channel, sender, 60, "Lien Détecté")
+    client.action(channel, "Pas de lien");
+  }
+    
   // If the command is known, let's execute it
   if (commandName === '!liens') {
     client.say(target, `Facebook: https://www.facebook.com/MasterSnakou | Twitter: https://twitter.com/MasterSnakou | Youtube: https://www.youtube.com/user/MasterSnakou | Youtube Replay: https://www.youtube.com/user/SnakouReplay | Instagram: https://www.instagram.com/mastersnakou | Snapchat: https://www.snapchat.com/add/mastersnakou`);
